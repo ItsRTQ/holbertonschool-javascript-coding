0c9fb594cde5270f8fd76ec92b98875fcd8b43af
William <titanbabrisa@gmail.com>
2024-05-14 00:19:15 -0400

    found antilles

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const request = require('request');
+const useURL = process.argv[2];
+const characterId = 'https://swapi-api.hbtn.io/api/people/18/';
+
+request(useURL, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(body);
+  const moviesInfo = data.results;
+  let foundAmount = 0;
+  for (let i = 0; i < moviesInfo.length; i++) {
+    if (moviesInfo[i].characters.includes(characterId)) {
+      foundAmount++;
+    }
+  }
+  console.log(foundAmount);
+});
