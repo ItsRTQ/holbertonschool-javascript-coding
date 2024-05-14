9b28a6e4599623607c8032f7d25b0e59550b116b
William <titanbabrisa@gmail.com>
2024-05-14 01:01:14 -0400

    scraping from random url

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const targetUrl = process.argv[2];
+const filePath = process.argv[3];
+
+request(targetUrl, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+  fs.writeFile(filePath, body, 'utf8', (err, text) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
