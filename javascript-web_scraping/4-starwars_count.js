1c86b75127b521793eead81ddf5d72d56afeca7e
William <titanbabrisa@gmail.com>
2024-05-14 00:31:43 -0400

    dont know

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@ const request = require('request');
 const useURL = process.argv[2];
 const characterId = 'https://swapi-api.hbtn.io/api/people/18/';
 
-request(useURL, function (err, res, body) {
+request(useURL, (err, res, body) => {
   if (err) {
     console.log(err);
   } else if (res.statusCode === 200) {
