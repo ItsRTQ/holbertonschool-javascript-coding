d6804f47a9df626219d8232210d4f0380f880a81
William <titanbabrisa@gmail.com>
2024-05-13 23:14:21 -0400

    starwars titles

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const request = require('request');
+const useURL = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+
+request(useURL, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(body);
+  console.log(data.title);
+});
