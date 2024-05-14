26cafa6331885654b39184fe1a353bfda352d3d3
William <titanbabrisa@gmail.com>
2024-05-14 00:27:34 -0400

    added invalid url handler

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,14 +6,17 @@ const characterId = 'https://swapi-api.hbtn.io/api/people/18/';
 request(useURL, function (err, res, body) {
   if (err) {
     console.log(err);
-  }
-  const data = JSON.parse(body);
-  const moviesInfo = data.results;
-  let foundAmount = 0;
-  for (let i = 0; i < moviesInfo.length; i++) {
-    if (moviesInfo[i].characters.includes(characterId)) {
-      foundAmount++;
+  } else if (res.statusCode === 200) {
+    const data = JSON.parse(body);
+    const moviesInfo = data.results;
+    let foundAmount = 0;
+    for (let i = 0; i < moviesInfo.length; i++) {
+      if (moviesInfo[i].characters.includes(characterId)) {
+        foundAmount++;
+      }
     }
+    console.log(foundAmount);
+  } else {
+    console.log('Invalid url');
   }
-  console.log(foundAmount);
 });
