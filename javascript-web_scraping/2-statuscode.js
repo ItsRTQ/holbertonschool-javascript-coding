51bd52ca71a3b1e5683c289b2f24f4137aaa55ae
William <titanbabrisa@gmail.com>
2024-05-13 23:01:47 -0400

    script to get url status

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const request = require('request');
+const targetURL = process.argv[2];
+
+request.get(targetURL).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
