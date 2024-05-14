0579a97ad1633a49a6242675c8b026e16b0be1e1
William <titanbabrisa@gmail.com>
2024-05-13 22:38:08 -0400

    script to write to file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const filePath = process.argv[2];
+const toAddText = process.argv[3];
+const fs = require('fs');
+fs.writeFile(filePath, toAddText, 'utf8', function (err, text) {
+  if (err) {
+    console.log(err);
+  }
+});
