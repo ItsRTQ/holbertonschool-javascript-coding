acd60883e0b9fffd1be700813fdc8dc3c1a7ca39
itsrtq <titanbabrisa@gmail.com>
2024-05-23 11:10:42 -0400

    wop

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 const readline = require('node:readline');
 const rl = readline.createInterface({
-    input:process.stdin,
-    output: process.stdout,
+  input:process.stdin,
+  output: process.stdout,
 });
 rl.question('Welcome to Holberton School, what is your name?\n', name => {
   console.log(`Your name is: ${name}`);
