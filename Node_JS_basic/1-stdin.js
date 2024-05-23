8bda589567a71062eb2f279fa9566680912681e8
itsrtq <titanbabrisa@gmail.com>
2024-05-23 11:15:47 -0400

    wop

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
 const readline = require('node:readline');
 const rl = readline.createInterface({
-  input:process.stdin,
-  output: process.stdout,
+    input:process.stdin,
+    output: process.stdout,
 });
-rl.question('Welcome to Holberton School, what is your name?', name => {
+rl.question('Welcome to Holberton School, what is your name?\n', name => {
   console.log(`Your name is: ${name}`);
   rl.close();
-  console.log('This important software is now closing');
+  console.log('This important software is now closing')
 });
