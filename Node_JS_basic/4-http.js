3c41d8c6d0ae215deeabeb9f3c3ee4af0bdd5509
William <titanbabrisa@gmail.com>
2024-05-25 14:11:51 -0400

    trying to fix requeirments

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,6 +3,6 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Holberton School!');
-}).listen(1245);
+}).listen(1245, '127.0.0.1');
 
 module.exports = app;
