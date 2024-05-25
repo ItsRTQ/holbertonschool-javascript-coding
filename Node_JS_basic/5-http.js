744e68224d8da22f3e59e5ee9817f12ea58fb8f9
William <titanbabrisa@gmail.com>
2024-05-25 14:24:18 -0400

    weee

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,18 +1,19 @@
 const http = require('http');
-const { readFile } = require('fs').promises;
+const countStudents = require('./3-read_file_async');
 
-const app = http.createServer(async (req, res) => {
+const app = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
 
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    try {
-      const data = await readFile('database.csv', 'utf8');
-      res.end(`This is the list of our students\n\n${data}`);
-    } catch (error) {
-      res.end('Cannot load the database');
-    }
+    countStudents('database.csv')
+      .then((data) => {
+        res.end(data);
+      })
+      .catch((error) => {
+        res.end('Cannot load the database');
+      });
   } else {
     res.end('Invalid endpoint');
   }
