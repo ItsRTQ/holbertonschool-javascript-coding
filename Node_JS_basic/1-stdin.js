c8b99e4e0c17319c5e891fbe6cbf7b0aa5ce6b18
itsrtq <titanbabrisa@gmail.com>
2024-05-25 12:57:31 -0400

    task 1 stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,8 @@
-const readline = require('node:readline');
-const rl = readline.createInterface({
-  input:process.stdin,
-  output: process.stdout,
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('name', (name) => {
+  process.stdout.write(`Your name is: ${name}`);
 });
-rl.question('Welcome to Holberton School, what is your name?\n', name => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
-  console.log('This important software is now closing')
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
