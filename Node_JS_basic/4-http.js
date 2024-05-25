6089cfc2aaab135d04e494036ab1f9f1326f4daf
William <titanbabrisa@gmail.com>
2024-05-25 14:07:18 -0400

    trying to fix requeirments

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -4,7 +4,6 @@ const app = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Holberton School!');
 });
-
 const port = 1245;
 app.listen(port);
 
