08e26d21ba3d986e41548273ca384b28b3eadd71
William <titanbabrisa@gmail.com>
2024-05-25 14:14:53 -0400

    trying to fix requeirments

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,6 +3,13 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Holberton School!');
-}).listen(1245, '127.0.0.1');
+});
+
+const port = 1245;
+const host = '127.0.0.1'
+
+app.listen(port, host, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
 
 module.exports = app;
