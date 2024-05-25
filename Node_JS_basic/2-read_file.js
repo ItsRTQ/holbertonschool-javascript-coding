c3d3a1e770fe189cb1f9ab94bc7584751f8b64ae
William <titanbabrisa@gmail.com>
2024-05-25 13:43:23 -0400

    work plz

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,38 +1,37 @@
 const fs = require('fs');
 
-function countStudents(fileName) {
-  const students = {};
-  const fields = {};
-  let length = 0;
+function countStudents(path) {
   try {
-    const fileData = fs.readFileSync(fileName, 'utf-8');
-    const lines = fileData.toString().split('\n');
-    for (let i = 0; i < lines.length; i += 1) {
-      if (lines[i]) {
-        length += 1;
-        const field = lines[i].toString().split(',');
-        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-          students[field[3]].push(field[0]);
-        } else {
-          students[field[3]] = [field[0]];
-        }
-        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-          fields[field[3]] += 1;
-        } else {
-          fields[field[3]] = 1;
-        }
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter(line => line.trim() !== '');
+    const countByField = {};
+
+    lines.forEach(line => {
+      const fields = line.split(',');
+      if (fields.length !== 2) return;
+
+      const [field, firstName] = fields;
+
+      if (!countByField[field]) {
+        countByField[field] = {
+          count: 1,
+          students: [firstName.trim()]
+        };
+      } else {
+        countByField[field].count++;
+        countByField[field].students.push(firstName.trim());
       }
+    });
+
+    for (const field in countByField) {
+      console.log(`Number of students in ${field}: ${countByField[field].count}. List: ${countByField[field].students.join(', ')}`);
     }
-    const totalNumberOfStudents = length - 1;
-    console.log(`Number of students: ${l}`);
-    for (const [key, value] of Object.entries(totalNumberOfStudents)) {
-      if (key !== 'field') {
-        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
-      }
-    }
+    const totalStudents = Object.values(countByField).reduce((total, field) => total + field.count, 0);
+    console.log(`Number of students: ${totalStudents}`);
   } catch (error) {
-    throw Error('Cannot load the database');
+    console.error('Cannot load the database');
   }
 }
-
+const databasePath = 'database.csv';
+countStudents(databasePath);
 module.exports = countStudents;
